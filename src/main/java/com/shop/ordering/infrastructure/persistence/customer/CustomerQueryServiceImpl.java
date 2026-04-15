6e1b6e2a947a7a509460c1e27adab35c40aebc39
chronosinfo <chronosinfo@chronosinfo.com.br>
2026-04-15 14:10:16 -0300

    finalizado aulas cqrs

--- src/main/java/com/shop/ordering/infrastructure/persistence/customer/CustomerQueryServiceImpl.java
+++ src/main/java/com/shop/ordering/infrastructure/persistence/customer/CustomerQueryServiceImpl.java
@@ -1,15 +1,27 @@
 package com.shop.ordering.infrastructure.persistence.customer;
 
+import com.shop.ordering.application.customer.query.CustomerFilter;
 import com.shop.ordering.application.customer.query.CustomerOutput;
 import com.shop.ordering.application.customer.query.CustomerQueryService;
+import com.shop.ordering.application.customer.query.CustomerSummaryOutput;
+import com.shop.ordering.application.order.query.CustomerMinimalOutput;
+import com.shop.ordering.application.order.query.OrderSummaryOutput;
 import com.shop.ordering.domain.model.customer.CustomerNotFoundException;
+import com.shop.ordering.infrastructure.persistence.order.OrderPersistenceEntity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -20,7 +32,7 @@ public class CustomerQueryServiceImpl implements CustomerQueryService {
     private final EntityManager entityManager;
 
     private static final String findByIdAsOutputJPQL = """
-            SELECT new com.algaworks.algashop.ordering.application.customer.query.CustomerOutput(
+            SELECT new com.shop.ordering.application.customer.query.CustomerOutput(
                 c.id,
                 c.firstName,
                 c.lastName,
@@ -33,7 +45,7 @@ public class CustomerQueryServiceImpl implements CustomerQueryService {
                 c.archivedAt,
                 c.promotionNotificationsAllowed,
                 c.archived,
-                new com.algaworks.algashop.ordering.application.commons.AddressData(
+                new com.shop.ordering.application.commons.AddressData(
                     c.address.street,
                     c.address.number,
                     c.address.complement,
@@ -55,5 +67,104 @@ public class CustomerQueryServiceImpl implements CustomerQueryService {
         } catch (NoResultException e) {
             throw new CustomerNotFoundException();
         }
+    }
+
+    @Override
+    public Page<CustomerSummaryOutput> filter(CustomerFilter filter) {
+
+        Long totalQueryResults = countTotalQueryResults(filter);
+
+        if (totalQueryResults.equals(0L)) {
+            PageRequest pageRequest = PageRequest.of(filter.getPage(), filter.getSize());
+            return new PageImpl<>(new ArrayList<>(), pageRequest, totalQueryResults);
+        }
+
+        return filterQuery(filter, totalQueryResults);
+    }
+
+    private Page<CustomerSummaryOutput> filterQuery(CustomerFilter filter, Long totalQueryResults) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<CustomerSummaryOutput> criteriaQuery = builder.createQuery(CustomerSummaryOutput.class);
+
+        Root<CustomerPersistenceEntity> root = criteriaQuery.from(CustomerPersistenceEntity.class);
+
+        criteriaQuery.select(
+                builder.construct(CustomerSummaryOutput.class,
+                        root.get("id"),
+                        root.get("firstName"),
+                        root.get("lastName"),
+                        root.get("email"),
+                        root.get("document"),
+                        root.get("phone"),
+                        root.get("birthDate"),
+                        root.get("loyaltyPoints"),
+                        root.get("registeredAt"),
+                        root.get("archivedAt"),
+                        root.get("promotionNotificationsAllowed"),
+                        root.get("archived")
+                )
+        );
+
+        Predicate[] predicates = toPredicates(builder, root, filter);
+        Order sortOrder = toSortOrder(builder, root, filter);
+
+        criteriaQuery.where(predicates);
+        if (sortOrder != null) {
+            criteriaQuery.orderBy(sortOrder);
+        }
+
+        TypedQuery<CustomerSummaryOutput> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        typedQuery.setFirstResult(filter.getSize() * filter.getPage());
+        typedQuery.setMaxResults(filter.getSize());
+
+        PageRequest pageRequest = PageRequest.of(filter.getPage(), filter.getSize());
+
+        return new PageImpl<>(typedQuery.getResultList(), pageRequest, totalQueryResults);
+    }
+
+    private Long countTotalQueryResults(CustomerFilter filter) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteriaQuery = builder.createQuery(Long.class);
+        Root<CustomerPersistenceEntity> root = criteriaQuery.from(CustomerPersistenceEntity.class);
+
+        Expression<Long> count = builder.count(root);
+        Predicate[] predicates = toPredicates(builder, root, filter);
+
+        criteriaQuery.select(count);
+        criteriaQuery.where(predicates);
+
+        TypedQuery<Long> query = entityManager.createQuery(criteriaQuery);
+
+        return query.getSingleResult();
+    }
+
+    private Order toSortOrder(CriteriaBuilder builder, Root<CustomerPersistenceEntity> root, CustomerFilter filter) {
+        String propertyName = filter.getSortByPropertyOrDefault().getPropertyName();
+
+        if (filter.getSortDirectionOrDefault() == Sort.Direction.ASC) {
+            return builder.asc(root.get(propertyName));
+        }
+
+        if (filter.getSortDirectionOrDefault() == Sort.Direction.DESC) {
+            return builder.desc(root.get(propertyName));
+        }
+
+        return null;
+    }
+
+    private Predicate[] toPredicates(CriteriaBuilder builder,
+                                     Root<CustomerPersistenceEntity> root, CustomerFilter filter) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (filter.getFirstName() != null && !filter.getFirstName().isBlank()) {
+            predicates.add(builder.like(builder.lower(root.get("firstName")), "%" + filter.getFirstName().toLowerCase() + "%"));
+        }
+
+        if (filter.getEmail() != null && !filter.getEmail().isBlank()) {
+            predicates.add(builder.like(builder.lower(root.get("email")), "%" + filter.getEmail().toLowerCase() + "%"));
+        }
+
+        return predicates.toArray(new Predicate[]{});
     }
 }
---
+++ src/main/java/com/shop/ordering/infrastructure/persistence/shoppingcart/ShoppingCartQueryServiceImpl.java
@@ -0,0 +1,34 @@
+package com.shop.ordering.infrastructure.persistence.shoppingcart;
+
+import com.shop.ordering.application.shoppingcart.query.ShoppingCartOutput;
+import com.shop.ordering.application.shoppingcart.query.ShoppingCartQueryService;
+import com.shop.ordering.application.utility.Mapper;
+import com.shop.ordering.domain.model.shoppingcart.ShoppingCartNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ShoppingCartQueryServiceImpl implements ShoppingCartQueryService {
+
+    private final ShoppingCartPersistenceEntityRepository persistenceRepository;
+    private final Mapper mapper;
+
+    @Override
+    public ShoppingCartOutput findById(UUID shoppingCartId) {
+        return persistenceRepository.findById(shoppingCartId)
+                .map(s -> mapper.convert(s, ShoppingCartOutput.class))
+                .orElseThrow(ShoppingCartNotFoundException::new);
+    }
+
+    @Override
+    public ShoppingCartOutput findByCustomerId(UUID customerId) {
+        return persistenceRepository.findByCustomer_Id(customerId)
+                .map(s -> mapper.convert(s, ShoppingCartOutput.class))
+                .orElseThrow(ShoppingCartNotFoundException::new);
+    }
+}
