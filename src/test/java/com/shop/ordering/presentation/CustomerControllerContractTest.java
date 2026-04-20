5b819a8059fe69b55253d2e16c8a37ad125c3db1
chronosinfo <chronosinfo@chronosinfo.com.br>
2026-04-20 13:16:16 -0300

    Usando Mock no teste de contrato

--- src/test/java/com/shop/ordering/presentation/CustomerControllerContractTest.java
+++ src/test/java/com/shop/ordering/presentation/CustomerControllerContractTest.java
@@ -1,24 +1,38 @@
 package com.shop.ordering.presentation;
 
+import com.shop.ordering.application.customer.management.CustomerInput;
+import com.shop.ordering.application.customer.management.CustomerManagementApplicationService;
+import com.shop.ordering.application.customer.query.CustomerOutput;
+import com.shop.ordering.application.customer.query.CustomerOutputTestDataBuilder;
+import com.shop.ordering.application.customer.query.CustomerQueryService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 @WebMvcTest(controllers = CustomerController.class)
 class CustomerControllerContractTest {
-    
+
     @Autowired
     private WebApplicationContext context;
+
+    @MockitoBean
+    private CustomerManagementApplicationService customerManagementApplicationService;
+
+    @MockitoBean
+    private CustomerQueryService customerQueryService;
 
     @BeforeEach
     public void setupAll() {
@@ -30,6 +44,13 @@ class CustomerControllerContractTest {
 
     @Test
     public void createCustomerContract() {
+        CustomerOutput customerOutput = CustomerOutputTestDataBuilder.existing().build();
+
+        Mockito.when(customerManagementApplicationService.create(Mockito.any(CustomerInput.class)))
+                .thenReturn(UUID.randomUUID());
+        Mockito.when(customerQueryService.findById(Mockito.any(UUID.class)))
+                .thenReturn(customerOutput);
+
         String jsonInput = """
         {
           "firstName": "John",
@@ -52,13 +73,13 @@ class CustomerControllerContractTest {
         """;
 
         RestAssuredMockMvc
-            .given()
+                .given()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .body(jsonInput)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
+                .when()
                 .post("/api/v1/customers")
-            .then()
+                .then()
                 .assertThat()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .statusCode(HttpStatus.CREATED.value())
