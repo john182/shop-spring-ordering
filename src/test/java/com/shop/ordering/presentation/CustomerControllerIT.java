48d97639cbce41919d83c7f9236b96c267437d4f
chronosinfo <chronosinfo@chronosinfo.com.br>
2026-04-20 13:04:11 -0300

    Testando endpoint de criação de cliente

---
+++ src/test/java/com/shop/ordering/presentation/CustomerControllerIT.java
@@ -0,0 +1,11 @@
+package com.shop.ordering.presentation;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class CustomerControllerIT {
+
+
+}
