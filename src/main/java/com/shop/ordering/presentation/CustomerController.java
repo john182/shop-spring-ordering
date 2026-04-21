66bfb8491f5cea6348b7fb23373af1177717ba98
chronosinfo <chronosinfo@chronosinfo.com.br>
2026-04-21 12:04:55 -0300

    Testes com paginação

--- src/main/java/com/shop/ordering/presentation/CustomerController.java
+++ src/main/java/com/shop/ordering/presentation/CustomerController.java
@@ -2,8 +2,10 @@ package com.shop.ordering.presentation;
 
 import com.shop.ordering.application.customer.management.CustomerInput;
 import com.shop.ordering.application.customer.management.CustomerManagementApplicationService;
+import com.shop.ordering.application.customer.query.CustomerFilter;
 import com.shop.ordering.application.customer.query.CustomerOutput;
 import com.shop.ordering.application.customer.query.CustomerQueryService;
+import com.shop.ordering.application.customer.query.CustomerSummaryOutput;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,6 +20,11 @@ public class CustomerController {
 
     private final CustomerManagementApplicationService customerManagementApplicationService;
     private final CustomerQueryService customerQueryService;
+
+    @GetMapping
+    public PageModel<CustomerSummaryOutput> findAll(CustomerFilter customerFilter) {
+        return PageModel.of(customerQueryService.filter(customerFilter));
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
